80dc5c605ac0ca95ffce1e8f7aad3b8d14e7a03e
Akshay S <akshayshan28@gmail.com>
2024-11-06 00:39:17 +0530

    refactor: remove photos and refactor name

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,13 +1,65 @@
 export const photos = [
-    { "name": "Sunset Serenade", "description": "A captivating scene of the sun bidding farewell to the day, painting the sky with hues of orange and pink.", "src": "/photos/1-min.jpg" , "texture": "/photos-rotated/1-min.jpg"  },
-    { "name": "Whispers of Autumn", "description": "Golden leaves dance in the crisp autumn breeze, whispering secrets of the changing season.", "src": "/photos/2-min.jpg"  , "texture": "/photos-rotated/2-min.jpg"},
-    { "name": "Ethereal Dreams", "description": "A surreal dreamscape where reality intertwines with imagination, inviting you to lose yourself in its beauty.", "src": "/photos/3-min.jpg" , "texture": "/photos-rotated/3-min.jpg" },
-    { "name": "Mystic Melodies", "description": "Melodies of the unknown echo through the mist, stirring curiosity and wonder in those who dare to listen.", "src": "/photos/4-min.jpg" , "texture": "/photos-rotated/4-min.jpg" },
-    { "name": "Serenity Falls", "description": "Water cascades gently over smooth rocks, creating a symphony of serenity in a secluded forest oasis.", "src": "/photos/5-min.jpg" , "texture": "/photos-rotated/5-min.jpg" },
-    { "name": "Enchanted Forest", "description": "Step into a realm where ancient trees whisper ancient tales and mystical creatures roam under the moon's soft glow.", "src": "/photos/6-min.jpg" , "texture": "/photos-rotated/6-min.jpg" },
-    { "name": "Celestial Symphony", "description": "Stars alight in the vast expanse of the night sky, composing a celestial symphony that resonates with the heart and soul.", "src": "/photos/7-min.jpg" ,  "texture": "/photos-rotated/7-min.jpg" },
-    { "name": "Ocean's Embrace", "description": "Feel the cool embrace of the ocean waves as they kiss the shore, carrying with them the secrets of the deep.", "src": "/photos/8-min.jpg" , "texture": "/photos-rotated/8-min.jpg" },
-    { "name": "Cityscape Reverie", "description": "Skyscrapers reach for the heavens, their lights shimmering like stars in a concrete constellation, illuminating the dreams of city dwellers below.", "src": "/photos/9-min.jpg" , "texture": "/photos-rotated/9-min.jpg" },
-    { "name": "Luminous Lullaby", "description": "Bathed in the gentle glow of the moonlight, a tranquil landscape beckons weary souls to rest and find solace in its soothing embrace.", "src": "/photos/10-min.jpg" ,  "texture": "/photos-rotated/10-min.jpg" }
-]
-
+  {
+    name: "Whispers of Autumn",
+    description:
+      "Golden leaves dance in the crisp autumn breeze, whispering secrets of the changing season.",
+    src: "/photos/2-min.jpg",
+    texture: "/photos-rotated/2-min.jpg",
+  },
+  {
+    name: "Ethereal Dreams",
+    description:
+      "A surreal dreamscape where reality intertwines with imagination, inviting you to lose yourself in its beauty.",
+    src: "/photos/3-min.jpg",
+    texture: "/photos-rotated/3-min.jpg",
+  },
+  {
+    name: "Mystic Melodies",
+    description:
+      "Melodies of the unknown echo through the mist, stirring curiosity and wonder in those who dare to listen.",
+    src: "/photos/4-min.jpg",
+    texture: "/photos-rotated/4-min.jpg",
+  },
+  {
+    name: "Serenity Falls",
+    description:
+      "Water cascades gently over smooth rocks, creating a symphony of serenity in a secluded forest oasis.",
+    src: "/photos/5-min.jpg",
+    texture: "/photos-rotated/5-min.jpg",
+  },
+  {
+    name: "Enchanted Forest",
+    description:
+      "Step into a realm where ancient trees whisper ancient tales and mystical creatures roam under the moon's soft glow.",
+    src: "/photos/6-min.jpg",
+    texture: "/photos-rotated/6-min.jpg",
+  },
+  {
+    name: "Celestial Symphony",
+    description:
+      "Stars alight in the vast expanse of the night sky, composing a celestial symphony that resonates with the heart and soul.",
+    src: "/photos/7-min.jpg",
+    texture: "/photos-rotated/7-min.jpg",
+  },
+  {
+    name: "Ocean's Embrace",
+    description:
+      "Feel the cool embrace of the ocean waves as they kiss the shore, carrying with them the secrets of the deep.",
+    src: "/photos/8-min.jpg",
+    texture: "/photos-rotated/8-min.jpg",
+  },
+  {
+    name: "Cityscape Reverie",
+    description:
+      "Skyscrapers reach for the heavens, their lights shimmering like stars in a concrete constellation, illuminating the dreams of city dwellers below.",
+    src: "/photos/9-min.jpg",
+    texture: "/photos-rotated/9-min.jpg",
+  },
+  {
+    name: "Luminous Lullaby",
+    description:
+      "Bathed in the gentle glow of the moonlight, a tranquil landscape beckons weary souls to rest and find solace in its soothing embrace.",
+    src: "/photos/10-min.jpg",
+    texture: "/photos-rotated/10-min.jpg",
+  },
+];
